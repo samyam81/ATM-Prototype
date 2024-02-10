ef7ebe160c3b867d7b25a4695a942148f29a4cc3
Samyam <139134689+samyam81@users.noreply.github.com>
2024-02-10 14:39:47 +0545

    Add files via upload

---
+++ EngENQ.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+
+public class EngENQ extends JFrame {
+        
+    JFrame frame;
+    JLabel bankLabel;
+    JLabel textfield=new JLabel();
+    double money;   
+     
+    Random rand=new Random();
+
+
+    Font myFont = new Font("Serrif", Font.ITALIC, 30);
+
+    public EngENQ(){
+                //Defining the frame
+        frame = new JFrame("Pawan Bank PVT LTD");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(750, 750);
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(new Color(0, 0,0));
+
+                // define the Main text;
+        bankLabel = new JLabel("You have:");
+        bankLabel.setFont(myFont);
+        bankLabel.setForeground(new Color(192,192,192));
+        bankLabel.setBounds(150, 100, 400, 50);
+
+                //defining the textfield
+        textfield.setBackground(new Color(229, 243, 214));
+        textfield.setForeground(new Color(255,0,0)); 
+        textfield.setBounds(170,160,300,50);
+        textfield.setFont(myFont);
+        textfield.setEnabled(false);
+        textfield.setOpaque(true);
+
+        money=rand.nextDouble(25000,50000);
+        textfield.setText("Rs: "+String.valueOf(money));
+        
+        frame.add(bankLabel);
+        frame.add(textfield);
+        frame.setVisible(true);
+    }
+    // public static void main(String[] args) {
+    //     new EngENQ();
+    // }
+}
