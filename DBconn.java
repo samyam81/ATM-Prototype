5d338fc8f614a10ab4eb0c2b23142f19b61e36e3
Samyam <139134689+samyam81@users.noreply.github.com>
2024-02-13 11:58:46 +0545

    Update DBconn.java

--- DBconn.java
+++ DBconn.java
@@ -1,20 +1,45 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBconn {
 
-    Connection connection;
-    Statement statement;
-    
-    public DBconn(){
-        try {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/BankATM";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "0000";
 
+  public Connection connection;
+    public Statement statement;
+
+    public DBconn() {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/BankATM","root","0000");
-            statement=connection.createStatement();
-        } catch (Exception e) {
-           e.printStackTrace();
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+            statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
+
+    public void close() {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
