4aa35876912bda3cca7bef4dac007d399ceded9e
Samyam <139134689+samyam81@users.noreply.github.com>
2024-02-15 14:31:52 +0545

    Update DBconn.java

--- DBconn.java
+++ DBconn.java
@@ -1,7 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DBconn {
 
@@ -9,32 +10,50 @@ public class DBconn {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "0000";
 
-  public Connection connection;
-    public Statement statement;
+    public Connection connection;
 
     public DBconn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-            statement = connection.createStatement();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public Connection getConnection() {
-        return connection;
+    public double getBalance(int pin, String customerID) {
+        double balance = 0.0;
+        String sqlQuery = "SELECT Balance FROM login10 WHERE PIN = ? AND CustomerID = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            preparedStatement.setInt(1, pin);
+            preparedStatement.setString(2, customerID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                balance = resultSet.getDouble("Balance");
+            } else {
+                System.out.println("No records found for PIN " + pin + " and CustomerID " + customerID);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return balance;
     }
 
-    public Statement getStatement() {
-        return statement;
+    public void updateBalance(int pin, String customerID, double newBalance) {
+        String updateQuery = "UPDATE login10 SET Balance = ? WHERE PIN = ? AND CustomerID = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+            preparedStatement.setDouble(1, newBalance);
+            preparedStatement.setInt(2, pin);
+            preparedStatement.setString(3, customerID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void close() {
         try {
-            if (statement != null) {
-                statement.close();
-            }
             if (connection != null) {
                 connection.close();
             }
